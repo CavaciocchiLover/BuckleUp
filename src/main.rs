e4a2c7f22be6458468389aac4453d269d500a484
Avolio <153174177+CavaciocchiLover@users.noreply.github.com>
2025-01-06 23:21:29 +0100

    Ho creato partenza,arrivo e cancella

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer};
+use actix_web::{delete, get, post, web, App, HttpResponse, HttpServer};
 use mongodb::bson::{doc, Document};
 use mongodb::{Client, Collection};
 use actix_cors::Cors;
@@ -7,10 +7,7 @@ use base64ct::{Base64, Encoding};
 use sha2::{Sha256, Digest};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::ops::Index;
-use std::path::Path;
+use sha2::digest::generic_array::arr;
 
 #[derive(Serialize, Deserialize)]
 struct Registrazione {
@@ -39,8 +36,8 @@ struct Viaggio {
     nomePacchetto: String,
     periodo: String,
     descrizione: String,
+    arrivo: String,
 }
-
 #[get("/listaviaggi")]
 async fn listaviaggi(client: web::Data<Client>) -> HttpResponse {
     let collection : Collection<Viaggio> = client.database("BuckleUp").collection("viaggi");
@@ -107,7 +104,7 @@ async fn login(client: web::Data<Client>, body: web::Bytes) -> HttpResponse {
 
                 if password == password_archiviata {
                     let json: Value = serde_json::from_str(&*("{\"ruolo\": \"".to_owned() + cursor.unwrap().get_str("ruolo").unwrap() + "\"}")).unwrap();
-                    return HttpResponse::Ok().json(json);
+                    HttpResponse::Ok().json(json)
                 } else {
                     HttpResponse::BadRequest().body("Password e/o email non sono validi")
                 }
@@ -118,7 +115,6 @@ async fn login(client: web::Data<Client>, body: web::Bytes) -> HttpResponse {
 }
 #[post("/nuovo")]
 async fn nuovo(client: web::Data<Client>, body: web::Bytes) -> HttpResponse {
-    println!("{:#?}", body);
     let json;
     match serde_json::from_slice::<Viaggio>(&body) {
         Ok(dati) => json = dati,
@@ -128,8 +124,7 @@ async fn nuovo(client: web::Data<Client>, body: web::Bytes) -> HttpResponse {
     let collection : Collection<Viaggio> = client.database("BuckleUp").collection("viaggi");
 
     match collection.insert_one(json).await {
-        Ok(some) => {
-            println!("{:#?}", some);
+        Ok(_) => {
             HttpResponse::Ok().finish()
         },
         Err(e) => HttpResponse::InternalServerError().body(e.to_string())
@@ -137,41 +132,102 @@ async fn nuovo(client: web::Data<Client>, body: web::Bytes) -> HttpResponse {
 }
 
 #[get("/paese")]
-async fn paese(lista: web::Data<Vec<String>>, query: web::Query<Value>) -> HttpResponse {
+async fn paese(lista: web::Data<Value> ,query: web::Query<Value>) -> HttpResponse {
     let nome = query.get("nome").unwrap_or(&Value::Null);
-
+    println!("{:#?}", query);
     if nome == &Value::Null {
         HttpResponse::BadRequest().body("Query non valido")
     } else {
-        let mut sx = 0;
-        let mut dx = lista.len() - 1;
-
-        while sx <= dx {
-            let cn = (sx + dx) / 2;
-            let riga: Vec<String> = lista[cn].split(';').collect();
-            match riga[1].cmp(nome.as_str().unwrap()) {
-                std::cmp::Ordering::Equal => return HttpResponse::Ok().body(riga[0]),
-                std::cmp::Ordering::Less => sx = cn + 1,
-                std::cmp::Ordering::Greater => {
-                    if cn == 0 {
-                        return HttpResponse::BadRequest().body("paese non valido")
-                    }
-                    dx = cn - 1;
-                },
-            }
-        }
-
-        //println!("{:#?}", paese);
-        //let sigla = paesi.get(nome.as_str().unwrap()).unwrap_or(&Value::Null);
-        /*if sigla == &Value::Null {
+        let codice = lista.get(nome.as_str().unwrap()).unwrap_or(&Value::Null);
+        if (codice) == &Value::Null {
             HttpResponse::BadRequest().body("Il paese non esiste")
         } else {
-            HttpResponse::Ok().body(paese)
+            HttpResponse::Ok().body(codice.to_string())
         }
-         */
-        HttpResponse::Ok().body("")
+
     }
 }
+
+#[delete("/cancella")]
+async fn cancella(client: web::Data<Client>, body: web::Bytes) -> HttpResponse {
+    let json;
+    match serde_json::from_slice::<Value>(&body) {
+        Ok(dati) => json = dati,
+        Err(_) => return HttpResponse::BadRequest().body("Il json non è valido")
+    }
+
+    let collection : Collection<Viaggio> = client.database("BuckleUp").collection("viaggi");
+
+    let nome = json.get("nome").unwrap_or(&Value::Null);
+
+    if nome == &Value::Null {
+        HttpResponse::BadRequest().body("Il json non è valido")
+    } else {
+        match collection.delete_one(doc!{"nomePacchetto": nome.as_str().unwrap()}).await {
+            Ok(_) => {
+                HttpResponse::Ok().finish()
+            },
+            Err(e) => HttpResponse::InternalServerError().body(e.to_string())
+        }
+    }
+}
+
+#[post("/modifica")]
+async fn modifica(client: web::Data<Client>, body: web::Bytes) -> HttpResponse {
+    let json;
+    match serde_json::from_slice::<Viaggio>(&body) {
+        Ok(dati) => json = dati,
+        Err(_) => return HttpResponse::BadRequest().body("Il json non è valido")
+    }
+
+    let collection : Collection<Viaggio> = client.database("BuckleUp").collection("viaggi");
+
+    match collection.replace_one(doc!{"nomePacchetto": &json.nomePacchetto}, json).await {
+        Ok(_) => {
+            HttpResponse::Ok().finish()
+        },
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string())
+    }
+}
+
+#[get("/arrivi")]
+async fn arrivi(client: web::Data<Client>) -> HttpResponse {
+    let collection : Collection<Value> = client.database("BuckleUp").collection("viaggi");
+
+    match collection.find(doc!{}).projection(doc!{"arrivo": 1, "_id": 0}).await {
+        Ok(mut cursor) => {
+            let mut result: Vec<String> = Vec::new();
+            while cursor.advance().await.unwrap_or(false) {
+                match cursor.deserialize_current() {
+                    Ok(document) => result.push(document["arrivo"].as_str().unwrap().to_string()),
+                    Err(e) => return HttpResponse::InternalServerError().body(e.to_string()),
+                }
+            }
+            HttpResponse::Ok().json(result)
+        },
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string())
+    }
+}
+
+#[get("/partenze")]
+async fn partenze(client: web::Data<Client>) -> HttpResponse {
+    let collection : Collection<Value> = client.database("BuckleUp").collection("viaggi");
+
+    match collection.find(doc!{}).projection(doc!{"partenza": 1, "_id": 0}).await {
+        Ok(mut cursor) => {
+            let mut result: Vec<Value> = Vec::new();
+            while cursor.advance().await.unwrap_or(false) {
+                match cursor.deserialize_current() {
+                    Ok(document) => result.push(document),
+                    Err(e) => return HttpResponse::InternalServerError().body(e.to_string()),
+                }
+            }
+            HttpResponse::Ok().json(result)
+        },
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string())
+    }
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -191,12 +247,7 @@ async fn main() -> std::io::Result<()> {
 
     let uri = "mongodb://127.0.0.1:27017/";
     let client = Client::with_uri_str(uri).await.expect("Unable to connect to MongoDB");
-    let mut paesi = Vec::<String>::new();
-    if let Ok(righe) = read_lines("paesi.txt") {
-        for riga in righe.flatten() {
-            paesi.push(riga);
-        }
-    }
+    let paesi: Value = serde_json::from_str(&fs::read_to_string("./paesi.json")?)?;
 
     println!("Il server è operativo al seguente ip: 127.0.0.1:8080");
 
@@ -210,20 +261,17 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(cors)
             .app_data(web::Data::new(client.clone()))
-            .app_data(web::Data::new(paesi))
+            .app_data(web::Data::new(paesi.clone()))
             .service(listaviaggi)
             .service(registrazione)
             .service(login)
             .service(nuovo)
             .service(paese)
+            .service(cancella)
+            .service(arrivi)
+            .service(partenze)
     })
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
-}
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
 }
